b581d74172d873c732e256986a82da46593d7c65
Bernardo Melo <bernardodmelo@gmail.com>
2020-11-03 16:54:44 +0000

    done w css

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +23,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI 
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', {beer: beersFromApi});
+    console.log('Beers from the database: ',  beersFromApi);
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/randombeers', (req, res) => {
+  punkAPI 
+  .getRandom()
+  .then(beersFromApi => {
+    res.render('randombeers', {beer: beersFromApi});
+    console.log('Beers from the database: ',  beersFromApi);
+  })
+  .catch(error => console.log(error));
+});
+
+app.get ('/beers/:id', (req, res)=> {
+  let id = req.params.id;
+  punkAPI 
+  .getBeer(id)
+  .then(beersFromApi => {
+    res.render('singlebeer', {beer: beersFromApi});
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
